95dbb3fd1747a54614dddd896f69f1cd75d80e44
Cyber <176347536+cyberspacepirates@users.noreply.github.com>
2024-12-06 11:16:23 -0300

    Updates recover.sh

--- recover.sh
+++ recover.sh
@@ -3,15 +3,16 @@ filename="wordlist.txt"
 SEED="magic canoe normal aware extend earn jewel fresh detail peanut dice viable"
 
 expected_address="bc1quhkf7wg82lamc0avjct0rt2s2k23wca4wtmnss"
+end_address="tmnss"
 
 electrum daemon -d 2> /dev/null
 
 while IFS= read -r line
 do
-  electrum restore "$SEED" -- passphrase "$line" > /dev/null
+  electrum restore "$SEED" --passphrase "$line" > /dev/null
   electrum load_wallet -w ~/.electrum/wallets/default_wallet > /dev/null
   address=$(electrum getunusedaddress)
-  if [ "$address" = "$expected_address" ]; then
+  if [["$address" == *"$end_address"]]; then
     echo "The wallet was found, the passphrase is: $line"
     break
   else
